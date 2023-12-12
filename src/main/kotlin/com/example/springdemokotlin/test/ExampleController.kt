6b1e8af1665c6b1311ce13d3005d3f2f2ab91f4b
dimassetyawan <dimasadriyantos@gmail.com>
2023-12-12 08:43:03 +0100

    Spring 3.2.0 error

--- src/main/kotlin/com/example/springdemokotlin/test/ExampleController.kt
+++ src/main/kotlin/com/example/springdemokotlin/test/ExampleController.kt
@@ -1,6 +1,5 @@
 package com.example.springdemokotlin.test
 
-
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
